cf2701f57ae33d6d021ce594f821859e6cf0e67f
Francisco Maciel <francisco.ma.maciel@gmail.com>
2016-02-25 18:11:21 +0000

    Change header and about sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,7 +14,7 @@ function handleTopNavAnimation() {
 	var top=$(window).scrollTop();
 
 	if(top>10){
-		$('#site-nav').addClass('navbar-solid'); 
+		$('#site-nav').addClass('navbar-solid');
 	}
 	else{
 		$('#site-nav').removeClass('navbar-solid'); 
